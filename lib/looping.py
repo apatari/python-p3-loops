1707c3a2439e9295a170199cb6ce4fdb2d8a0f65
Andy Patari <andy.patari@gmail.com>
2023-09-13 08:52:02 -0400

    passing tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    n = 10
+    while n>0 :
+        print(n)
+        if n ==1 :
+            print("Happy New Year!")
+        n -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    res = [int * int for int in int_list]
+    return res
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    def fizzer(n):
+        if  n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 5 == 0:
+            print("Buzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        else:
+            print(n)
+    for i in range(1, 101):
+        fizzer(i)
